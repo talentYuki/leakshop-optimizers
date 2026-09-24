75991275eb3e2d5ea521cbbe6201f413330662d8
talentYuki <egerone@inbox.ru>
2026-09-25 01:31:02 +0700

    Add Order route

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { HashRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Guides from './Guides'
+import Order from './Order'
+import './index.css'
 
-<HashRouter>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/guides" element={<Guides />} />
-    <Route path="/order" element={<Order />} />
-  </Routes>
-</HashRouter>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/guides" element={<Guides />} />
+        <Route path="/order" element={<Order />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>,
+)
